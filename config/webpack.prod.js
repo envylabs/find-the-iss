1d67f66543a5a2c8f68a7b5ec1faac67c1d1a1fe
Drew Powers <drew@pow.rs>
2017-12-08 11:36:45 -0500

    Cache bust

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,14 +1,19 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const merge = require('webpack-merge');
 
 const common = require('./webpack.common.js');
 
 module.exports = merge.smart(common, {
+  output: {
+    filename: '[name].js',
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production'),
       },
     }),
+    new ExtractTextPlugin('styles.[chunkhash].css'),
   ],
 });
