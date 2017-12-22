f74eab5cc421fea18af4a403be23f891b1b30b03
Drew Powers <drew@pow.rs>
2017-12-22 14:21:20 -0500

    Add about text, fix globe sizing

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React from "react";
+import { connect } from "react-redux";
 
-import { closeInfo, openInfo } from 'components/actions';
-import Close from 'components/Close';
-import Question from 'components/Question';
+import { closeInfo, openInfo } from "components/actions";
+import Close from "components/Close";
+import Question from "components/Question";
 
 const Info = ({ dispatch, ...props }) => (
   <div className="info">
-    <button className="info-button info-open" onClick={() => dispatch(openInfo())}>
+    <button
+      className="info-button info-open"
+      onClick={() => dispatch(openInfo())}
+    >
       <Question />
     </button>
-    {props.isInfoOpen &&
+    {props.isInfoOpen && (
       <div>
         <div className="info-fullscreen">
           <button className="info-button" onClick={() => dispatch(closeInfo())}>
@@ -19,28 +22,54 @@ const Info = ({ dispatch, ...props }) => (
           <h2 className="info-heading">About This Project</h2>
           <div className="info-body">
             <p>
-              Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam
-              id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit. Curabitur blandit tempus
-              porttitor.
+              Find the ISS is a WebAR experiment that relies on the open-source{" "}
+              <a
+                href="http://wheretheiss.at/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Where The ISS At?
+              </a>{" "}
+              API to track the ISS. The WebAR concepts are custom-built using{" "}
+              <a
+                href="https://github.com/mrdoob/three.js/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Three.js
+              </a>{" "}
+              and WebRTC, but concepts were pulled from{" "}
+              <a
+                href="https://github.com/jeromeetienne/ar.js"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                AR.js
+              </a>.
             </p>
             <p>
-              Cras justo odio, dapibus ac facilisis in, egestas eget quam. Nullam
-              id dolor id nibh ultricies vehicula ut id elit. Lorem ipsum dolor
-              sit amet, consectetur adipiscing elit. Curabitur blandit tempus
-              porttitor.
+              Made by{" "}
+              <a
+                href="https://envylabs.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Envy Labs
+              </a>.
             </p>
-            <p>
-              Built by Envy Labs with help from...
-            </p>
+            <p>Browsers supported:</p>
+            <ul>
+              <li>iOS 11</li>
+              <li>Chrome Android</li>
+            </ul>
           </div>
         </div>
       </div>
-    }
+    )}
   </div>
 );
 
 export default connect(
   state => ({ isInfoOpen: state.isInfoOpen }),
-  dispatch => ({ dispatch }),
+  dispatch => ({ dispatch })
 )(Info);
